f59f3c343c7c540613db4d1ac79ee5643f3fa2ee
bbgsm <teowlzovr@gmail.com>
2024-09-19 16:35:38 +0800

    偏移注释

--- cheat/offsets.h
+++ cheat/offsets.h
@@ -1,17 +1,18 @@
 #pragma once
 // version: v3.0.80.16
-#define OFF_GAME_STATE 0x1EB73F0    // 游戏是否运行 bool,bytes: 40A3899D3A26A000 3C24002F00032E92 4402C6674402CC4C 4402CCCD4402C99A 4402CC4C3F57AD7B 000000004402D5BD 000000403C2399A8 0000000100000000 3D4CCCCD000028DF
+
+/*** r5apex.exe Offset  ***/
+#define OFF_GAME_STATE 0x1EB73F0    // 对局是否进行 bool,bytes: 40A3899D3A26A000 3C24002F00032E92 4402C6674402CC4C 4402CCCD4402C99A 4402CC4C3F57AD7B 000000004402D5BD 000000403C2399A8 0000000100000000 3D4CCCCD000028DF
 #define OFF_ENTITY_LIST  0x1F22028  // 对象数组(玩家，物品) 大小10000 * 32
 #define OFF_LOCAL_PLAYER 0x23F52B8  // 操作玩家指针
 #define OFF_NAME_LIST 0xD3E54D0     // 名称字典指针
-
 #define OFF_MATRIX1 0x2422fD0       // 4x4矩阵 float[16]
 #define OFF_LEVEL_NAME 0x17F66C4     // 对局名称
+/*** r5apex.exe Offset ***/
 
+/*** Object、Player ***/
 #define OFF_VISIBLE_TIME 0x19A0     // 最后可见时间 float(一直可见数值一直会增大)
-
 #define OFF_ITEM_ID 0x1568          // 物品id int
-
 #define OFF_BONES 0xDF8             // 骨骼指针
 #define OFF_STUDIO_HDR 0x1000
 #define OFF_INDEX_IN_NAMELIST 0x38  // 名称下标
@@ -32,3 +33,4 @@
 #define OFF_VIEW_ANGLES5 0x2AD0
 #define OFF_VIEW_ANGLES6 0x1EEF
 #define OFF_VIEW_ANGLES7 0x2514
+/*** Object、Player Offset ***/
